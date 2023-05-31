399fef9789f6545cb108312c531eefb21b268bf0
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-05-31 19:23:37 +0200

    Added user data model

---
+++ src/main/java/cc/projectnexus/adapters/java/datamodels/User.java
@@ -0,0 +1,10 @@
+package cc.projectnexus.adapters.java.datamodels;
+
+import java.sql.Timestamp;
+
+public class User {
+	private String id;
+	private String[] identifiers;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+}
