b88589441f6cf64f98ad5035dcab8a5846a64047
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-02 12:38:39 +0200

    Fix a error in intellij which is real annoying

--- src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
+++ src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
@@ -3,7 +3,7 @@ package cc.projectnexus.adapters.java.component;
 public class GuildComponent {
 
 	public static int getAmountOfGuilds(String token) {
-		return true; // TODO: Implement
+		return 0;
 	}
 
 }
