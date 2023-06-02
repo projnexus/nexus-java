ff822dc80bca53feb8f06be77e3e63726af9eed1
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-02 14:40:16 +0200

    Fixed authorization

--- src/main/java/cc/projectnexus/adapters/java/component/AuthorizeComponent.java
+++ src/main/java/cc/projectnexus/adapters/java/component/AuthorizeComponent.java
@@ -4,6 +4,8 @@ import cc.projectnexus.adapters.java.NexusApi;
 import cc.projectnexus.adapters.java.route.Method;
 import cc.projectnexus.adapters.java.route.Route;
 
+import java.io.IOException;
+
 public class AuthorizeComponent {
 
 	private static String BASE_API = "https://api.projectnexus.cc/";
@@ -15,7 +17,11 @@ public class AuthorizeComponent {
 	 * @see cc.projectnexus.adapters.java.NexusClient instead of handling your authorization yourself.
 	 */
 	public static boolean authorizeToken(String token) {
-		return NexusApi.attempt(token); // TODO: Implement
+		try {
+			return NexusApi.attempt(token); // TODO: Implement
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
