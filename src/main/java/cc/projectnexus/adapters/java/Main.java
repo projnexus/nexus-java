fa51e5639bc20c667c3fac27a5897856a66160c7
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-03 14:36:24 +0200

    More debugging

--- src/main/java/cc/projectnexus/adapters/java/Main.java
+++ src/main/java/cc/projectnexus/adapters/java/Main.java
@@ -4,7 +4,11 @@ import cc.projectnexus.adapters.java.component.GuildComponent;
 import cc.projectnexus.adapters.java.datamodels.GuildSettings;
 import cc.projectnexus.adapters.java.datamodels.Region;
 import cc.projectnexus.adapters.java.exceptions.TokenNotAuthorizedException;
+import cc.projectnexus.adapters.java.route.Route;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Instant;
 
@@ -23,7 +27,21 @@ public class Main extends NexusClient {
 	@Override
 	public void authSuccess() {
 		GuildSettings publish = new GuildSettings("1078849224067776552", "1078849224067776552", true, true, "", Timestamp.from(Instant.now()), Timestamp.from(Instant.now()), new Region[]{Region.EUROPE});
-		GuildComponent.placeGuildSettings(publish);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String guildJson;
+		try {
+			guildJson = mapper.writeValueAsString(publish);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+
+		try {
+			NexusRequester.sendPostRequest("https://api.projectnexus.cc/guild", guildJson, getInstance().getProperties().getToken());
+			System.out.println(guildJson);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
