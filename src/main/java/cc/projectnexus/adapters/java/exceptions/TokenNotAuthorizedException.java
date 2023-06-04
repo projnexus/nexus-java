37e24d252a651414960c7afb519949bae0e3fd7a
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-04 23:09:43 +0200

    Delete TokenNotAuthorizedException.java

--- src/main/java/cc/projectnexus/adapters/java/exceptions/TokenNotAuthorizedException.java
+++
@@ -1,7 +0,0 @@
-package cc.projectnexus.adapters.java.exceptions;
-
-public class TokenNotAuthorizedException extends Exception {
-	public TokenNotAuthorizedException(String message) {
-		super("The provided token was never authorized.");
-	}
-}
