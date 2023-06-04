30bee6516bf08f7d0b8b2d5e0b724b1777284559
Sean Spirins <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-04 21:37:14 +0100

    Update GuildComponent.java

--- src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
+++ src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
@@ -2,86 +2,86 @@ package cc.projectnexus.adapters.java.component;
 
 import cc.projectnexus.adapters.java.adapter.JsonAdapter;
 import cc.projectnexus.adapters.java.datamodels.GuildSettings;
+import cc.projectnexus.adapters.java.datamodels.Infraction;
 import cc.projectnexus.adapters.java.datamodels.requests.create.GuildCreateRequest;
 import cc.projectnexus.adapters.java.datamodels.requests.patch.GuildPatchRequest;
 import cc.projectnexus.adapters.java.exceptions.TokenNotAuthorizedException;
 import cc.projectnexus.adapters.java.request.NexusRequest;
 import cc.projectnexus.adapters.java.request.RequestResponse;
 import cc.projectnexus.adapters.java.route.Method;
+import cc.projectnexus.adapters.java.route.Route;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GuildComponent {
 
 	/**
-	 * Returns the amount of registered guilds that has a Guild-Settings data model.
+	 * Returns the amount of registered guilds that have a Guild data model.
+	 *
 	 * @return The amount of registered guilds.
-	 * @throws TokenNotAuthorizedException If the token is not properly authorized, or is missing something.
- 	 */
-	public static int getGuildAmount() {
-		NexusRequest request = new NexusRequest(Method.GET, "https://api.projectnexus.cc/guilds/count");
+	 */
+	public static int getAmountOfGuilds() {
+		NexusRequest request = new NexusRequest(Method.GET, Route.GuildsRoutes.GET_AMOUNT_COUNT);
 		RequestResponse response = request.execute();
-		JsonAdapter jsonAdapter = new JsonAdapter();
-
-		if (response.getResponse().isEmpty()) {
-			return 0;
-		} else {
-			String json = response.getResponse();
-			Gson gson = new Gson();
-
-			try {
-				JsonElement jsonElement = gson.fromJson(json, JsonElement.class);
-				JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-				int amount = jsonObject.get("count").getAsInt();
-
-				return amount;
-			} catch (Exception e) {
-				return 0;
-			}
-		}
+		if (response.getResponseCode() != 200) throw new RuntimeException("Something went wrong while getting the amount of guilds.");
+		return new Gson().fromJson(response.getResponse(), JsonObject.class).get("count").getAsInt();
 	}
 
-	public static GuildSettings getGuild(String id) {
-		NexusRequest request = new NexusRequest(Method.GET, "https://api.projectnexus.cc/guild/" + id);
-		RequestResponse res = request.execute();
-
-		JsonAdapter jsonAdapter = new JsonAdapter();
-
-		GuildSettings guildSettings = jsonAdapter.fromJson(res.getResponse(), GuildSettings.class);
-
-		if (guildSettings != null) {
-			return guildSettings;
-		} else {
-			return null;
-		}
+	/**
+	 * Search the database for a guilds with a given ID
+	 * @param id The guild's Discord ID
+	 * @return The guilds that was found.
+	 */
+	public static GuildSettings getGuild (String id) {
+		NexusRequest request = new NexusRequest(Method.GET, Route.GuildsRoutes.GET_DATA_SINGLE + id);
+		RequestResponse response = request.execute();
+		if (response.getResponseCode() != 200) throw new RuntimeException("Something went wrong while getting the guild.");
+		return new Gson().fromJson(response.getResponse(), GuildSettings.class);
 	}
 
-	public static RequestResponse addGuildSettings(GuildCreateRequest req) {
-		JsonAdapter adapter = new JsonAdapter();
-		String json = adapter.toJson(req);
-
-		NexusRequest request = new NexusRequest(Method.POST, "https://api.projectnexus.cc/guild", json);
-		RequestResponse res = request.execute();
-
-		return res;
+	/**
+	 * Create a new guild in the database.
+	 * @param data The data to pass
+	 * @return The created guild.
+	 */
+	public static GuildSettings createGuild(JsonObject data) {
+		NexusRequest request = new NexusRequest(Method.POST, Route.GuildsRoutes.POST_CREATE_NEW, data.toString());
+		RequestResponse response = request.execute();
+		if (response.getResponseCode() != 200) throw new RuntimeException("Something went wrong while creating the guild.");
+		return new Gson().fromJson(response.getResponse(), GuildSettings.class);
 	}
 
-	public static RequestResponse deleteGuild(String guildId) {
-		NexusRequest request = new NexusRequest(Method.DELETE, "https://api.projectnexus.cc/guild/" + guildId);
-		RequestResponse res = request.execute();
-		return res;
+	/**
+	 * Delete a guild from the database.
+	 * @param id The ID of the guild.
+	 * @return If the guild was deleted successfully.
+	 */
+	public static boolean deleteGuild(String id) {
+		NexusRequest request = new NexusRequest(Method.DELETE, Route.GuildsRoutes.DELETE_GUILD + id);
+		RequestResponse response = request.execute();
+		if (response.getResponseCode() != 200) throw new RuntimeException("Something went wrong while deleting the guild.");
+		return true;
 	}
 
-	public static RequestResponse patchGuild(String id, GuildPatchRequest req) {
-		JsonAdapter adapter = new JsonAdapter();
-		String json = adapter.toJson(req);
-
-		NexusRequest request = new NexusRequest(Method.PATCH, "https://api.projectnexus.cc/guild/" + id, json);
-		RequestResponse res = request.execute();
-
-		return res;
+	/**
+	 * Update a guild in the database
+	 * @param guild The guild to update.
+	 * @return The updated guild.
+	 */
+	public static GuildSettings updateGuild(GuildSettings guild) {
+		JsonObject data = new JsonObject();
+		data.addProperty("auto_ban", guild.isAutoBan());
+		data.addProperty("auto_unban", guild.isAutoUnban());
+		data.addProperty("logs_channel", guild.getLogsChannel());
+		data.add("enabled_regions", new Gson().toJsonTree(guild.getEnabledRegions()));
+		NexusRequest request = new NexusRequest(Method.PUT, Route.GuildsRoutes.PUT_UPDATE_GUILD + guild.getGuildId(), data.toString());
+		RequestResponse response = request.execute();
+		if (response.getResponseCode() != 200) throw new RuntimeException("Something went wrong while updating the guild.");
+		return new Gson().fromJson(response.getResponse(), GuildSettings.class);
 	}
 
 }
