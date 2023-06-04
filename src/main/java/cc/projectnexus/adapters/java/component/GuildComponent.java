de167e68ce28d8a8b232c933750e83b535a05011
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-04 12:44:11 +0200

    JSON Binding implementation

--- src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
+++ src/main/java/cc/projectnexus/adapters/java/component/GuildComponent.java
@@ -6,15 +6,11 @@ import cc.projectnexus.adapters.java.datamodels.GuildSettings;
 import cc.projectnexus.adapters.java.exceptions.TokenNotAuthorizedException;
 import cc.projectnexus.adapters.java.route.Method;
 import cc.projectnexus.adapters.java.route.Route;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GuildComponent {
 
 	/**
 	 * Returns the amount of registered guilds that has a Guild-Settings data model.
-	 * @param token The API access token of yours, recommended to be passed in from your {@link cc.projectnexus.adapters.java.NexusClient} class.
 	 * @return The amount of registered guilds.
 	 * @throws TokenNotAuthorizedException If the token is not properly authorized, or is missing something.
  	 */
@@ -37,30 +33,7 @@ public class GuildComponent {
 	}
 
 	public static void placeGuildSettings(GuildSettings settings) {
-		ObjectMapper mapper = new ObjectMapper();
-		String guildSettingsJson;
-		try {
-			guildSettingsJson = mapper.writeValueAsString(settings);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode jsonNode;
-		try {
-			jsonNode = objectMapper.readTree(guildSettingsJson);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-
-		System.out.println(guildSettingsJson);
-
-		try {
-			String response = NexusApi.provideRequest(Method.POST, guildSettingsJson, Route.GuildsRoutes.POST_CREATE_NEW);
-			System.out.println(response);
-		} catch (TokenNotAuthorizedException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 }
