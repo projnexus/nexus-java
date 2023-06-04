4b944cd071884ab5ad6dcb615ba4e6e115188c66
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-04 23:11:54 +0200

    Something got broken

--- src/main/java/cc/projectnexus/adapters/java/datamodels/GuildSettings.java
+++ src/main/java/cc/projectnexus/adapters/java/datamodels/GuildSettings.java
@@ -2,34 +2,18 @@ package cc.projectnexus.adapters.java.datamodels;
 
 import lombok.*;
 
-import javax.json.bind.annotation.JsonbProperty;
 import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 public class GuildSettings {
-    @JsonbProperty("id")
     private String id;
-
-    @JsonbProperty("guildId")
     private String guildId;
-
-    @JsonbProperty("auto_ban")
     private boolean autoBan;
-
-    @JsonbProperty("auto_unban")
     private boolean autoUnban;
-
-    @JsonbProperty("logs_channel")
     private String logsChannel;
-
-    @JsonbProperty("createdAt")
     private Timestamp createdAt;
-
-    @JsonbProperty("updatedAt")
     private Timestamp lastUpdated;
-
-    @JsonbProperty("enabledRegions")
     private Region[] enabledRegions;
 
     public GuildSettings() {
