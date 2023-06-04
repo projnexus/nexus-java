6cca52ff8a34be2968f47e0fe01ac9cb59b7b6f8
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-06-04 23:09:38 +0200

    Removed authorization

--- src/main/java/cc/projectnexus/adapters/java/client/NexusClient.java
+++ src/main/java/cc/projectnexus/adapters/java/client/NexusClient.java
@@ -8,58 +8,16 @@ import lombok.Setter;
 @Setter
 public abstract class NexusClient {
 	protected static NexusClient instance;
-	protected boolean authorize;
 	protected NexusClientProperties properties;
 
 	/**
 	 * Create a new constructor of NexusClient.
-	 * @param authorize If you want to authorize or not.
 	 * @param properties The properties.
-	 */
-	public NexusClient(boolean authorize, NexusClientProperties properties) {
-		instance = this;
-		this.authorize = authorize;
-		this.properties = properties;
-		authorize();
-	}
-
-	/**
-	 * Automatically enables authorize and provides the properties object.
-	 * @param properties The properties object.
 	 */
 	public NexusClient(NexusClientProperties properties) {
 		instance = this;
-		this.authorize = true;
 		this.properties = properties;
-		authorize();
-	}
-
-	/**
-	 * Create a new constructor of NexusClient.
-	 */
-	public NexusClient() {
-		instance = this;
-		this.authorize = false;
-		this.properties = new NexusClientProperties("", false);
-	}
-
-	/**
-	 * Handle the authorization to the API.
-	 * @return Returning if the authorization worked our not.
-	 */
-	protected boolean authorize() {
-		boolean result = false;
-		if (authorize) {
-			result = AuthorizeComponent.authorizeToken();
-			if (result) {
-				System.out.println("Authorized Nexus Wrapper.");
-				authSuccess();
-			} else {
-				System.out.println("Could not authorize to Nexus Wrapper.");
-				authFailed();
-			}
-		}
-		return result;
+		run();
 	}
 
 	/**
@@ -77,6 +35,5 @@ public abstract class NexusClient {
 		return instance;
 	}
 
-	public abstract void authSuccess();
-	public abstract void authFailed();
+	public abstract void run();
 }
--- src/main/java/cc/projectnexus/adapters/java/component/AuthorizeComponent.java
+++ src/main/java/cc/projectnexus/adapters/java/component/AuthorizeComponent.java
@@ -1,27 +1,5 @@
 package cc.projectnexus.adapters.java.component;
 
-import cc.projectnexus.adapters.java.api.NexusApi;
-import cc.projectnexus.adapters.java.client.NexusClient;
-
-import java.io.IOException;
-
-/**
- * Handle the authorization of API keys.
- */
 public class AuthorizeComponent {
-
-	/**
-	 * Authorize the token in the API.
-	 * @return Rather or not the authorization was completed.
-	 * @see NexusClient instead of handling your authorization yourself.
-	 */
-	public static boolean authorizeToken() {
-		final String token = NexusClient.getInstance().getProperties().getToken();
-		try {
-			return NexusApi.attempt(token);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
+	// TODO: Later
 }
