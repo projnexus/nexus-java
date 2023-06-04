84bb0e38f5efc292052de93b6aa7400afe00d25f
Sean Spirins <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-04 21:37:53 +0100

    Debugging

--- src/main/java/cc/projectnexus/adapters/java/Debug.java
+++ src/main/java/cc/projectnexus/adapters/java/Debug.java
@@ -1,26 +1,16 @@
 package cc.projectnexus.adapters.java;
 
-import cc.projectnexus.adapters.java.adapter.JsonAdapter;
 import cc.projectnexus.adapters.java.client.NexusClient;
 import cc.projectnexus.adapters.java.client.NexusClientProperties;
 import cc.projectnexus.adapters.java.component.GuildComponent;
 import cc.projectnexus.adapters.java.component.InfractionComponent;
-import cc.projectnexus.adapters.java.component.UserComponent;
-import cc.projectnexus.adapters.java.datamodels.GuildSettings;
+
+import java.util.Arrays;
+import java.util.List;
+
 import cc.projectnexus.adapters.java.datamodels.Infraction;
 import cc.projectnexus.adapters.java.datamodels.Region;
-import cc.projectnexus.adapters.java.datamodels.requests.create.InfractionCreateRequest;
-import cc.projectnexus.adapters.java.request.NexusRequest;
-import cc.projectnexus.adapters.java.request.RequestResponse;
-import cc.projectnexus.adapters.java.route.Method;
-import jakarta.json.bind.Jsonb;
-import jakarta.json.bind.JsonbConfig;
-import org.eclipse.yasson.JsonBindingProvider;
-
-import java.sql.Array;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Arrays;
+import com.google.gson.*;
 
 public class Debug extends NexusClient {
 	private String token = "BOT 2OAln4KjAoeO9KfFPaEcijZ2YsBotqccnGEMz18H2g6p175Cevm2Ujp0DOzwab6L";
@@ -36,7 +26,30 @@ public class Debug extends NexusClient {
 
 	@Override
 	public void authSuccess() {
-		System.out.println(UserComponent.getUser("eb18aac9-7cd0-483e-8c91-939eaba34e98", null));
+//		// create a new object with gson
+//		JsonObject object = new JsonObject();
+//		object.addProperty("reason", "test");
+//		object.addProperty("proof", "test");
+//		object.addProperty("executor", 1);
+//
+//		// create a new array of region
+//		JsonArray regions = new JsonArray();
+//		regions.add("europe");
+//		regions.add("na");
+//
+//		// create a new array of identifiers
+//		JsonArray identifiers = new JsonArray();
+//		identifiers.add("steam:123456789");
+//
+//		// add the regions and identifiers to the object
+//		object.add("regions", regions);
+//		object.add("identifiers", identifiers);
+
+		// create a new infraction
+//		JsonObject guildSettings = new JsonObject();
+//		guildSettings.addProperty("id", "1");
+
+
 	}
 
 	@Override
